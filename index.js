4017e99dfce6b8a4d42c08f82c94b85dd49c5443
WildCard65 <lollol222gg@gmail.com>
2020-08-14 22:06:03 -0400

    Swapped to using async functions.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const node_path = require('path')
-const child_process = require('child_process')
+const { spawn } = require('child_process')
 
 const ga_io = require('@actions/io');
 const ga_core = require('@actions/core');
+
+const action_dir = process.env.GITHUB_WORKSPACE;
 
 function getBoolean(value) {
     switch (value) {
@@ -18,90 +20,110 @@ function getBoolean(value) {
     }
 }
 
-{
-    const install_ambuild = getBoolean(ga_core.getInput('install-ambuild', { required: true }));
-    if (install_ambuild) {
-        const ambuild_path = node_path.join(process.env.GITHUB_WORKSPACE, 'ambuild');
+function failTheAction(error) {
+    ga_core.setFailed(error);
+    process.exit(1);
+}
 
-        ga_core.startGroup('Installing AMBuild from GitHub');
+function onChildProcessExit(childProcess) {
+    return new Promise((resolve, reject) => {
+        childProcess.on('exit', (exitCode, signal) => {
+            if (exitCode != 0) {
+                if (exitCode == null) {
+                    if (signal == null)
+                        reject(new Error('Cloning of AMBuild has failed for an unknown reason'));
+                    else
+                        reject(new Error('Cloning of AMBuild has terminated by a signal: ' + signal));
+                }
+                else
+                    reject(new Error('Cloning of AMBuild has failed with exit code: ' + exitCode));
+            }
+            else
+                resolve(undefined);
+        });
+        childProcess.on('error', (errorObj) => { reject(errorObj); });
+    });
+}
 
-        var cmd_res = child_process.spawnSync('git', ['clone', 'https://github.com/alliedmodders/ambuild.git', ambuild_path], {
-            cwd: process.env.GITHUB_WORKSPACE,
-            stdio: [process.stdin, process.stdout, process.stderr],
-            shell: true,
+async function installAMBuild() {
+    const ambuild_dir = node_path.join(action_dir, 'ambuild');
+
+    {
+        ga_core.info("Cloning AMBuild from 'https://github.com/alliedmodders/ambuild.git'");
+        const clone_proc = spawn('git', ['clone', 'https://github.com/alliedmodders/ambuild.git', ambuild_dir], {
+            cwd: action_dir,
+            stdio: ['inherit', 'inherit', 'inherit'],
+            detached: false,
+            shell: false,
+            windowsVerbatimArguments: true,
             windowsHide: true
         });
-        if (cmd_res.status != 0) {
-            if (cmd_res.status == null)
-                ga_core.setFailed('Cloning of AMBuild failed with signal: ' + cmd_res.signal);
-            else
-                ga_core.setFailed('Cloning of AMBuild failed with error code: ' + cmd_res.signal);
-        }
 
-        cmd_res = child_process.spawnSync('pip', ['install', ambuild_path], {
-            cwd: ambuild_path,
-            stdio: [process.stdin, process.stdout, process.stderr],
-            shell: true,
+        await onChildProcessExit(clone_proc).catch(failTheAction);
+    }
+
+    {
+        ga_core.info('Installing AMBuild with PIP');
+        const install_proc = spawn('pip', ['install', ambuild_dir], {
+            cwd: action_dir,
+            stdio: ['inherit', 'inherit', 'inherit'],
+            detached: false,
+            shell: false,
+            windowsVerbatimArguments: true,
             windowsHide: true
         });
-        if (cmd_res.status != 0) {
-            if (cmd_res.status == null)
-                ga_core.setFailed('Installation of AMBuild failed with signal: ' + cmd_res.signal);
-            else
-                ga_core.setFailed('Installation of AMBuild failed with error code: ' + cmd_res.signal);
-        }
 
-        ga_core.endGroup();
+        await onChildProcessExit(install_proc).catch(failTheAction);
     }
 }
 
-const build_folder = ga_core.getInput('build-path', { required: true });
-const project_root = ga_core.getInput('project-root', { required: true });
 
-ga_core.startGroup('Building the project');
-
-const build_dir = node_path.join(process.env.GITHUB_WORKSPACE, build_folder);
-ga_io.mkdirP(build_dir).catch((error) => { ga_core.setFailed(error) });
-
-const configure_script = node_path.join(process.env.GITHUB_WORKSPACE, project_root, 'configure.py');
-var configure_args = ga_core.getInput('args', { required: false });
+if (getBoolean(ga_core.getInput('install-ambuild', { required: true })))
+    installAMBuild();
 
 
-var py_args = [configure_script]
-if (configure_args != null && configure_args != '')
-    py_args = py_args.concat(configure_args.split(' '))
+async function buildProject() {
+    const build_folder = ga_core.getInput('build-path', { required: true });
+    const project_root = ga_core.getInput('project-root', { required: true });
 
-cmd_res = child_process.spawnSync('python', py_args, {
-    cwd: build_dir,
-    stdio: [process.stdin, process.stdout, process.stderr],
-    shell: true,
-    windowsHide: true
-});
-if (cmd_res.status != 0) {
-    if (cmd_res.status == null)
-        ga_core.setFailed("Invocation of 'configure.py' failed with signal:" + cmd_res.signal);
-    else
-        ga_core.setFailed("Invocation of 'configure.py' failed with error code: " + cmd_res.status);
+    const build_dir = node_path.join(action_dir, build_folder);
+    const configure_script = node_path.join(action_dir, project_root, 'configure.py');
+
+    ga_io.mkdirP(build_dir).catch(failTheAction);
+
+    var python_args = [configure_script];
+    {
+        var script_args = ga_core.getInput('args', { required: false });
+        if (script_args != null && script_args != '')
+            python_args = python_args.concact(script_args.split(' '));
+    }
+
+    console.log('Configuring the project for building');
+    const configure_proc = spawn('python', python_args, {
+        cwd: build_dir,
+        stdio: ['inherit', 'inherit', 'inherit'],
+        detached: false,
+        shell: false,
+        windowsVerbatimArguments: true,
+        windowsHide: true
+    });
+    await onChildProcessExit(configure_proc).catch(failTheAction);
+
+    console.log('Executing the build process');
+    const build_proc = spawn('ambuild', undefined, {
+        cwd: build_dir,
+        stdio: ['inherit', 'inherit', 'inherit'],
+        detached: false,
+        shell: false,
+        windowsVerbatimArguments: true,
+        windowsHide: true
+    });
+    await onChildProcessExit(build_proc).catch(failTheAction);
+
+    if (getBoolean(ga_core.getInput('delete-build', { required: false }))) {
+        console.log('Deleting the build output')
+        ga_io.rmRF(build_dir).catch((error) => { ga_core.setFailed(error) });
+    }
 }
 
-cmd_res = child_process.spawnSync('ambuild', [], {
-    cwd: build_dir,
-    stdio: [process.stdin, process.stdout, process.stderr],
-    shell: true,
-    windowsHide: true
-});
-if (cmd_res.status != 0) {
-    if (cmd_res.status == null)
-        ga_core.setFailed('Build failed with signal: ' + cmd_res.signal);
-    else
-        ga_core.setFailed('Build failed with error code: ' + cmd_res.signal);
-}
-
-
-ga_core.endGroup();
-
-const delete_build = getBoolean(ga_core.getInput('delete-build', { required: false }))
-if (delete_build) {
-    console.log('Deleting the build output')
-    ga_io.rmRF(build_dir).catch((error) => { ga_core.setFailed(error) });
-}
+buildProject();
